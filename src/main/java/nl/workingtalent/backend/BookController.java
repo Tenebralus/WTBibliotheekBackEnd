e95e6fab2647b04008497f40c4f73f643e304e8d
Tenebralus <legendariuszz@hotmail.com>
2023-01-05 15:35:05 +0100

    Added some queries to book

---
+++ src/main/java/nl/workingtalent/backend/BookController.java
@@ -0,0 +1,49 @@
+package nl.workingtalent.backend;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(maxAge = 3600)
+public class BookController {
+
+	@Autowired
+	private IBookRepository repo;
+	
+	@RequestMapping(value = "book/all")
+	public List<Book> findAllBooks()
+	{
+		return repo.findAll();
+	}
+	
+	@RequestMapping(value = "book/id/{id}")
+	public Book findById(@PathVariable long id)
+	{
+		return repo.findById(id).get();
+	}
+	
+	@RequestMapping(value = "book/title/{title}")
+	public List<Book> findByTitle(@PathVariable String title)
+	{
+		return repo.findByTitle(title);
+	}
+	
+	@RequestMapping(value = "book/isbn/{isbn}")
+	public List<Book> findByIsbn(@PathVariable String isbn)
+	{
+		return repo.findByIsbn(isbn);
+	}
+	
+	@RequestMapping(value = "book/create", method = RequestMethod.POST)
+	public void createBook(@RequestBody Book book)
+	{
+		repo.save(book);
+	}
+}
