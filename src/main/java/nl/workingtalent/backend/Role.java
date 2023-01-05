75abbc19d63439996d6a8cd00fea37cce9328fbe
Tenebralus <legendariuszz@hotmail.com>
2023-01-05 12:10:17 +0100

    Added Role and User

---
+++ src/main/java/nl/workingtalent/backend/Role.java
@@ -0,0 +1,42 @@
+package nl.workingtalent.backend;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(length = 50, nullable = false)
+	private String type;
+	
+	@ManyToMany
+	private List<User> users;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
