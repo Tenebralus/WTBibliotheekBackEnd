538d0cd20e14fb6e6866c938338d5aea33afa6c4
lwaw <laurensedwards@hotmail.com>
2023-01-05 12:11:28 +0100

    Added Loan and BookCopy classes

---
+++ src/main/java/nl/workingtalent/backend/BookCopy.java
@@ -0,0 +1,73 @@
+package nl.workingtalent.backend;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class BookCopy {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable=false)
+	private int bookCopyNr;
+	
+	@Column(nullable=false)
+	private String status;
+	
+	@ManyToOne
+	private Book book;
+	
+	@OneToMany(mappedBy="bookCopy")
+	private List<Loan> loans;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getBookCopyNr() {
+		return bookCopyNr;
+	}
+
+	public void setBookCopyNr(int bookCopyNr) {
+		this.bookCopyNr = bookCopyNr;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public List<Loan> getLoans() {
+		return loans;
+	}
+
+	public void setLoans(List<Loan> loans) {
+		this.loans = loans;
+	}
+	
+	
+}
