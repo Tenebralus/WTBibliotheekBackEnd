e4dbda359fafcc75e367adf2a6df273d15bb1a59
Tenebralus <legendariuszz@hotmail.com>
2023-01-06 10:24:30 +0100

    added user find methods

--- src/main/java/nl/workingtalent/backend/Controllers/UserController.java
+++ src/main/java/nl/workingtalent/backend/Controllers/UserController.java
@@ -1,5 +1,61 @@
 package nl.workingtalent.backend.Controllers;
 
-public class UserController {
+import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import nl.workingtalent.backend.Entities.User;
+import nl.workingtalent.backend.Repositories.IUserRepository;
+
+public class UserController {
+	
+	IUserRepository repo;
+	
+	@RequestMapping(value = "user/all")
+	public List<User> findAllUsers()
+	{
+		return repo.findAll();
+	}
+	
+	@RequestMapping(value = "user/id/{id}")
+	public User findUserById(@PathVariable long id)
+	{
+		return repo.findById(id).get();
+	}
+	
+	@RequestMapping(value = "user/firstname/{firstName}")
+	public List<User> findByFirstName(@PathVariable String firstName)
+	{
+		return repo.findByFirstName(firstName);
+	}
+	
+	@RequestMapping(value = "user/lastname/{lastName}")
+	public List<User> findByLastName(String lastName)
+	{
+		return repo.findByLastName(lastName);
+	}
+	
+	@RequestMapping(value = "user/emailaddress/{emailAddress}")
+	public List<User> findByEmailAddress(String emailAddress)
+	{
+		return repo.findByEmailAddress(emailAddress);
+	}
+	
+	@RequestMapping(value = "user/dateaccountcreated/{dateAccountCreated}")
+	public List<User> findByDateAccountCreated(LocalDateTime dateAccountCreated)
+	{
+		return repo.findByDateAccountCreated(dateAccountCreated);
+				
+	}
+	
+	@RequestMapping(value = "user/active/{active}")
+	public List<User> findByActive(boolean active)
+	{
+		return repo.findByActive(active);
+	}
+	
+	// add create, update and delete and link with frontend
+	// being able to login
 }
--- src/main/java/nl/workingtalent/backend/Repositories/IUserRepository.java
+++ src/main/java/nl/workingtalent/backend/Repositories/IUserRepository.java
@@ -1,5 +1,16 @@
 package nl.workingtalent.backend.Repositories;
 
-public interface IUserRepository {
+import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import nl.workingtalent.backend.Entities.User;
+
+public interface IUserRepository extends JpaRepository<User, Long>{ 
+	List<User> findByFirstName(String firstName);
+	List<User> findByLastName(String lastName);
+	List<User> findByEmailAddress(String emailAddress);
+	List<User> findByDateAccountCreated(LocalDateTime dateAccountCreated);
+	List<User> findByActive(boolean active);
 }
