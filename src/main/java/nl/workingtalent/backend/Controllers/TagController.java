6c0b9bf00466f95891aafebf02c3d81d0d0f67b7
Tenebralus <legendariuszz@hotmail.com>
2023-01-09 15:25:32 +0100

    (BW-139) added CRUD for Tag

---
+++ src/main/java/nl/workingtalent/backend/Controllers/TagController.java
@@ -0,0 +1,62 @@
+package nl.workingtalent.backend.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import nl.workingtalent.backend.Entities.Tag;
+import nl.workingtalent.backend.Repositories.ITagRepository;
+
+@RestController
+@CrossOrigin(maxAge = 3600)
+public class TagController {
+	
+	@Autowired
+	private ITagRepository repo;
+	
+	@RequestMapping(value = "tag/all")
+	public List<Tag> findAllTags()
+	{
+		return repo.findAll();
+	}
+	
+	@RequestMapping(value = "tag/id/{id}")
+	public Tag findById(@PathVariable long id)
+	{
+		return repo.findById(id).get();
+	}
+	
+	@RequestMapping(value = "tag/name/{name}")
+	public List<Tag> findByName(@PathVariable String name)
+	{
+		return repo.findByName(name);
+	}
+	
+	@RequestMapping(value = "tag/create", method = RequestMethod.POST)
+	public void createTag(@RequestBody Tag tag)
+	{
+		repo.save(tag);
+	}
+	
+	@RequestMapping(value = "tag/update/{id}", method = RequestMethod.PUT)
+	public void updateTag(@PathVariable long id, @RequestBody Tag tag )
+	{
+		Tag foundTag = findById(id);
+		foundTag.setName(tag.getName());
+		foundTag.setBooks(tag.getBooks());
+		
+		repo.save(tag);
+	}
+	
+	@RequestMapping(value = "tag/delete/{id}", method = RequestMethod.DELETE)
+	public void deleteTag(@PathVariable long id)
+	{
+		repo.deleteById(id);
+	}
+}
---
+++ src/main/java/nl/workingtalent/backend/Repositories/ITagRepository.java
@@ -0,0 +1,11 @@
+package nl.workingtalent.backend.Repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import nl.workingtalent.backend.Entities.Tag;
+
+public interface ITagRepository extends JpaRepository<Tag, Long>{
+	List<Tag> findByName(String name);
+}
