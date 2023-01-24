0ab6a81d009764edebb70bd1fc566a90cdcaec71
lwaw <laurensedwards@hotmail.com>
2023-01-24 15:40:36 +0100

    added broken email functionality

---
+++ src/main/java/nl/workingtalent/backend/Controllers/EmailController.java
@@ -0,0 +1,38 @@
+package nl.workingtalent.backend.Controllers;
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import nl.workingtalent.backend.EmailService;
+
+//import de.svenjacobs.loremipsum.LoremIpsum;
+
+@RestController
+public class EmailController {
+
+	@Autowired
+	private EmailService emailService;
+
+	@GetMapping("/sendemail")
+	public void sendEmail() {
+		//LoremIpsum loremIpsum = new LoremIpsum();
+
+		this.emailService.sendSimpleMessage("noreply@demo.nl", "to@demo.nl", "Demo bericht", "test");
+	}
+/*
+	@GetMapping("/sendemailwithattachment")
+	public void sendEmailWithAttachment() {
+		LoremIpsum loremIpsum = new LoremIpsum();
+
+		try {
+			this.emailService.sendMessageWithAttachment("noreply@demo.nl", "to@demo.nl", "Demo bericht", loremIpsum.getParagraphs(2));
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+*/
+}
